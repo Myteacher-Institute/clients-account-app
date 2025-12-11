2300b8ac4f9dec17c51ef6acf5de11309869f229
Richard-Raph <richardakpan77@gmail.com>
2025-12-11 15:56:37 +0100

    `Update AccountScreen.jsx to use Alert for logout confirmation`

--- app/screens/dashboard/account/AccountScreen.jsx
+++ app/screens/dashboard/account/AccountScreen.jsx
@@ -1,4 +1,5 @@
 import { colors } from '@/theme';
+import { Alert } from 'react-native';
 import { clearToken } from '@/auth/token';
 import { useUser } from '@/context/UserContext';
 import AccountInfo from './components/AccountInfo';
@@ -8,15 +9,24 @@ import { ClientsButton, ClientsLayout } from '@/components';
 const AccountScreen = ({ navigation }) => {
     const { setUser } = useUser();
 
-    const handleLogout = async () => {
-        try {
-            await clearToken();
-            setUser(null);
-            navigation.reset({ index: 0, routes: [{ name: 'SigninScreen' }] });
-        } catch (error) {
-            console.error('[Logout Error]', error);
-        }
-    };
+    const handleLogout = () => Alert.alert(
+        'Confirm Logout',
+        'Are you sure you want to log out?',
+        [
+            { text: 'Cancel', style: 'cancel' },
+            {
+                text: 'Logout', style: 'destructive', onPress: async () => {
+                    try {
+                        await clearToken();
+                        setUser(null);
+                        navigation.reset({ index: 0, routes: [{ name: 'SigninScreen' }] });
+                    } catch (error) {
+                        console.error('[Logout Error]', error);
+                    }
+                },
+            },
+        ]
+    );
 
     return (
         <ClientsLayout>
